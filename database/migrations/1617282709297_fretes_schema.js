436bf33b4d96e67b7fa526c267900db1c1333900
Pedro Vitor da Costa Silva <70039868+iPedroV@users.noreply.github.com>
2021-04-01 11:14:13 -0300

    Add files via upload
    Arquivo completo

---
+++ database/migrations/1617282709297_fretes_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class FretesSchema extends Schema {
+  up () {
+    this.create('fretes', (table) => {
+      table.increments()
+      table.decimal('valor').notNullable()
+      table.integer('quantidade_passageiro').notNullable()
+      table.date('data_frete').notNullable()
+      table.decimal('hora_partida').notNullable()
+      table.decimal('hora_chegada').notNullable()
+      table.integer('cliente_id').references('id').inTable('clientes').unsigned().notNullable()
+      table.integer('aeronave_id').references('id').inTable('aeronaves').unsigned().notNullable()
+      table.integer('aeroporto_partida_id').references('id').inTable('aeroportos').unsigned().notNullable()
+      table.integer('aeroporto_destino_id').references('id').inTable('aeroportos').unsigned().notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('fretes')
+  }
+}
+
+module.exports = FretesSchema
